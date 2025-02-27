3a36a432f1135a1f78a29e8c53a233ec7d5b890f
Rahul Sharma <rahul2022@glbajajgroup.org>
2025-02-27 12:43:20 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";  // treat all JS as a newer version
+
+//alert(3 + 3) // alert we are using node.js/not chrome browser
+
+console.log(3 + 3);
+
+let name = "rahul"
+let age = 18
+let isLogedIn = false
+
+
+//bigint
+//string
+//boolean => true,false
+//null => standalone value
+//undefine
+//symbol => uniqness
+
+console.log(typeof "rahul");
+console.log(typeof age);
+console.log(typeof undefined);
+
+
+
+
+
