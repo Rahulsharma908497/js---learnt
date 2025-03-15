ed7714f9dfe67b8e46ef8eb56e4911d045c2171b
Rahul Sharma <rahul2022@glbajajgroup.org>
2025-03-15 11:07:39 +0000

    conversion in string to number

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,15 @@
+let isLoggedIn = true
+
+let booleanIsLoggedIn = Number (isLoggedIn)   // output 1
+let num = "rahul"
+let is = Number(num)
+// console.log(typeof is)
+// console.log( is)
+// console.log( booleanIsLoggedIn);
+// 1 -> true || 0 -> false
+// "" -> false
+// "Rahul"-> true
+let someNumber = 36
+let stringNumber = String(someNumber)
+let ans = Number(stringNumber)
+console.log(typeof ans)
